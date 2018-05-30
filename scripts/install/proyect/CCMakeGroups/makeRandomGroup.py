7a54f6796e4c335dd2f73cfcc5713f61a9c13815
DavidIMSER <d.medina@imserltda.com>
2018-05-30 11:14:27 -0400

    update training random split by LOU

--- scripts/install/proyect/CCMakeGroups/makeRandomGroup.py
+++ scripts/install/proyect/CCMakeGroups/makeRandomGroup.py
@@ -55,72 +55,92 @@ class makeGroup(object):
         process = procesFile.processDataSet(dirDataNormal, 'pathOutput', ',')
         process.processMatrixData()
         ListResultAlgorithm = []
-        #aplicamos naiveBayes
-        print "Apply GaussianNB"
-        naiveBayesGB = naiveBayes.naiveBayes(process.matrixData)
-        naiveBayesGB.applyAlgorithmGaussian()
-        ListResultAlgorithm.append(naiveBayesGB.performanceValues)
+        try:
+            #aplicamos naiveBayes
+            print "Apply GaussianNB"
+            naiveBayesGB = naiveBayes.naiveBayes(process.matrixData)
+            naiveBayesGB.applyAlgorithmGaussian()
+            ListResultAlgorithm.append(naiveBayesGB.performanceValues)
+        except:
+            pass
 
-        print "Apply BernoulliNB"
-        naiveBayesBB = naiveBayes.naiveBayes(process.matrixData)
-        naiveBayesBB.applyAlgorithmBernoulliNB()
-        ListResultAlgorithm.append(naiveBayesBB.performanceValues)
-
+        try:
+            print "Apply BernoulliNB"
+            naiveBayesBB = naiveBayes.naiveBayes(process.matrixData)
+            naiveBayesBB.applyAlgorithmBernoulliNB()
+            ListResultAlgorithm.append(naiveBayesBB.performanceValues)
+        except:
+            pass
         #aplicamos SVC
         for kernel in ['linear', 'poly', 'rbf', 'sigmoid']:
-            print "Apply SVC %s" % kernel
-            SVCValues = SVC.SVCObjet(process.matrixData, kernel)
-            SVCValues.applyAlgorithm()
-            ListResultAlgorithm.append(SVCValues.performanceValues)
-
+            try:
+                print "Apply SVC %s" % kernel
+                SVCValues = SVC.SVCObjet(process.matrixData, kernel)
+                SVCValues.applyAlgorithm()
+                ListResultAlgorithm.append(SVCValues.performanceValues)
+            except:
+                pass
         #aplicamos randomForest
         for criterion in ['gini', 'entropy']:
             for estimator in [10, 20, 50, 100, 150, 200, 250, 500, 750, 1000, 1500]:
-                print "Apply randomForest %s, %d" % (criterion, estimator)
-                randomForestValues = randomForest.randomForest(process.matrixData, criterion, estimator)
-                randomForestValues.applyAlgorithm()
-                ListResultAlgorithm.append(randomForestValues.performanceValues)
-
+                try:
+                    print "Apply randomForest %s, %d" % (criterion, estimator)
+                    randomForestValues = randomForest.randomForest(process.matrixData, criterion, estimator)
+                    randomForestValues.applyAlgorithm()
+                    ListResultAlgorithm.append(randomForestValues.performanceValues)
+                except:
+                    pass
         #aplicamos NuSVC
         for kernel in ['linear', 'poly', 'rbf', 'sigmoid']:
-            print "Apply NuSVC %s" % kernel
-            nuSVCValues = nuSVC.nuSVC(process.matrixData, kernel)
-            nuSVCValues.applyAlgorithm()
-            ListResultAlgorithm.append(nuSVCValues.performanceValues)
-
+            try:
+                print "Apply NuSVC %s" % kernel
+                nuSVCValues = nuSVC.nuSVC(process.matrixData, kernel)
+                nuSVCValues.applyAlgorithm()
+                ListResultAlgorithm.append(nuSVCValues.performanceValues)
+            except:
+                pass
         #aplicamos arboles de decision...
         for estimator in [10, 20, 50, 100, 150, 200, 250, 500, 750, 1000, 1500]:
-            print "Apply GradientBoostingClassifier %d" % estimator
-            gradientTreeBoostValue = gradientTreeBoost.gradientTreeBoost(process.matrixData, estimator)
-            gradientTreeBoostValue.applyAlgorithm()
-            ListResultAlgorithm.append(gradientTreeBoostValue.performanceValues)
-
+            try:
+                print "Apply GradientBoostingClassifier %d" % estimator
+                gradientTreeBoostValue = gradientTreeBoost.gradientTreeBoost(process.matrixData, estimator)
+                gradientTreeBoostValue.applyAlgorithm()
+                ListResultAlgorithm.append(gradientTreeBoostValue.performanceValues)
+            except:
+                pass
         #aplicamos arboles de decision...
         for criterion in ['gini', 'entropy']:
             for splitter in ['best', 'random']:
-                print "Apply DecisionTreeClassifier %s %s" % (criterion, splitter)
-                decisionTreesValue = decisionTrees.decisionTrees(process.matrixData, criterion, splitter)
-                decisionTreesValue.applyAlgorithm()
-                ListResultAlgorithm.append(decisionTreesValue.performanceValues)
-
+                try:
+                    print "Apply DecisionTreeClassifier %s %s" % (criterion, splitter)
+                    decisionTreesValue = decisionTrees.decisionTrees(process.matrixData, criterion, splitter)
+                    decisionTreesValue.applyAlgorithm()
+                    ListResultAlgorithm.append(decisionTreesValue.performanceValues)
+                except:
+                    pass
         #aplicamos adaBoost...
         for algorithm in ['SAMME', 'SAMME.R']:
             for estimator in [10, 20, 50, 100, 150, 200, 250, 500, 750, 1000, 1500]:
-                print "Apply adaBoost: %d, %s" % (estimator, algorithm)
-                adaBoostValue = adaBoost.adaBoostLOU(process.matrixData, algorithm, estimator)
-                adaBoostValue.applyAlgorithm()
-                ListResultAlgorithm.append(adaBoostValue.performanceValues)
-
+                try:
+                    print "Apply adaBoost: %d, %s" % (estimator, algorithm)
+                    adaBoostValue = adaBoost.adaBoostLOU(process.matrixData, algorithm, estimator)
+                    adaBoostValue.applyAlgorithm()
+                    ListResultAlgorithm.append(adaBoostValue.performanceValues)
+                except:
+                    pass
 
         #aplicamos knn...
         for algorithm in ['auto', 'ball_tree', 'kd_tree', 'brute']:
             for weight in ['uniform', 'distance']:
                 for metric in ['minkowski', 'euclidean']:
                     for i in range(2, 5):
-                        print "Apply KNN: %d, %s, %s, %s" % (i, algorithm, weight, metric)
-                        knn = knnAlgorithm.knnAlgorithmLOU(process.matrixData, algorithm, weight, metric, i)
-                        knn.applyAlgorithm()
-                        ListResultAlgorithm.append(knn.performanceValues)
+                        try:
+                            print "Apply KNN: %d, %s, %s, %s" % (i, algorithm, weight, metric)
+                            knn = knnAlgorithm.knnAlgorithmLOU(process.matrixData, algorithm, weight, metric, i)
+                            knn.applyAlgorithm()
+                            ListResultAlgorithm.append(knn.performanceValues)
+                        except:
+                            pass
         #procesamos la salida...
         nameFile = "performanceTrainingWithLOU.csv"
         #procesamos los resultados...
